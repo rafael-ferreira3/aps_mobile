8755aeac0486ed25cbc28cfc0d07f225cfb1beae
Rafael Ferreira <rafaelpcf3@gmail.com>
2020-11-07 13:55:47 -0300

    Corrigido conversão Base64, arrumado imports

--- lib/helpers/ImageHelper.dart
+++ lib/helpers/ImageHelper.dart
@@ -2,6 +2,11 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 Uint8List imageFromBase64String(String base64String) {
-  String imgBase64 = base64String.split(',')[1];
+  String imgBase64;
+  if(base64String.contains(",")){
+    imgBase64 = base64String.split(',')[1];
+  } else {
+    imgBase64 = base64String;
+  }
   return base64Decode(imgBase64);
 }
--- lib/view/screens/base/base_screen.dart
+++ lib/view/screens/base/base_screen.dart
@@ -1,7 +1,7 @@
-import 'package:aps_mobile/view/tabs/home/home_tab.dart';
 import 'package:aps_mobile/view/tabs/category/category_tab.dart';
+import 'package:aps_mobile/view/tabs/home/home_tab.dart';
 import 'package:aps_mobile/view/tabs/pedido/pedido_tab.dart';
-import 'file:///C:/aps/aps_mobile/lib/view/tabs/places/places_tab.dart';
+import 'package:aps_mobile/view/tabs/places/places_tab.dart';
 import 'package:mobx/mobx.dart';
 import 'package:aps_mobile/store/page_store.dart';
 import 'package:flutter/material.dart';
